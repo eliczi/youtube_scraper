6ab3575655408739046aa0500793fbee09a16360
eliczi <eliczeus@gmail.com>
2022-05-09 14:44:07 +0200

    first_commit

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+path = "C:\\Users\Adam\Documents\GitHub\youtube_scraper\chromedriver.exe"
+video = "https://www.youtube.com/watch?v=U6gbGk5WPws"
+driver = webdriver.Chrome(path)
+driver.get(video)
+driver.implicitly_wait(10)
+x = driver.find_element(by=By.CSS_SELECTOR,
+                        value="[aria-label='Accept the use of cookies and other data for the purposes described']")
+x.click()
+button = driver.find_element(by=By.CLASS_NAME, value='ytp-ad-skip-button-container')
+button.click()
+x = driver.find_element(by=By.CSS_SELECTOR, value="[aria-label='Skip trial']")
+x.click()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def scroll_to_bottom():
+    page_eng = False
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+    while not page_eng:
+        driver.execute_script("window.scrollTo(0, " + str(last_height) + ");")
+        time.sleep(1)
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if last_height == new_height:
+            page_eng = True
+        else:
+            last_height = new_height
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def scroll_num_times(num):
+    for _ in range(num):
+        height = driver.execute_script("return document.documentElement.scrollHeight")
+        driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+        time.sleep(1)
