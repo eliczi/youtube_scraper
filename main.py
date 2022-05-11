671860b0060199e28328f2c6f496e0adf97e4146
eliczi <eliczeus@gmail.com>
2022-05-11 07:23:48 +0200

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import pandas as pd
 
 path = "C:\\Users\Adam\Documents\GitHub\youtube_scraper\chromedriver.exe"
-video = "https://www.youtube.com/watch?v=tBQEONNKU6U"
+video = "https://www.youtube.com/watch?v=bSCsmBkGzc4"
 driver = webdriver.Chrome(path)
 driver.get(video)
 driver.implicitly_wait(25)
@@ -15,11 +15,12 @@ x = driver.find_element(by=By.CSS_SELECTOR,
                         value="[aria-label='Accept the use of cookies and other data for the purposes described']")
 x.click()
 
+
 # button = driver.find_element(by=By.CLASS_NAME, value='ytp-ad-skip-button-container')
 # button.click()
-x = driver.find_element(by=By.CSS_SELECTOR, value="[aria-label='Skip trial']")
-x.click()
-
+# x = driver.find_element(by=By.CSS_SELECTOR, value="[aria-label='Skip trial']")
+# x.click()
+#
 
 def scroll_to_bottom():
     page_eng = False
@@ -41,7 +42,7 @@ def scroll_num_times(num):
         time.sleep(1)
 
 
-scroll_num_times(1)
+scroll_to_bottom()
 data = []
 likes = []
 
@@ -50,13 +51,19 @@ for comment in WebDriverWait(driver, 15).until(EC.presence_of_all_elements_locat
 
 # likes = driver.find_elements_by_xpath('//*[@id="vote-count-middle"]')
 # for like in likes:
-#     if 'K' in like.text:
-#         x = like.text.replace('K', '')
-#         x = float(x) * 1000
-#         x = int(x)
-#         print(x)
+#     x = ''
+#     if like.text == '':
+#         x = 0
+#     else:
+#         if 'K' in like.text:
+#             x = like.text.replace('K', '')
+#             x = float(x) * 1000
+#             x = int(x)
+#         else:
+#             x = int(like.text)
+#     likes.append(x)
+
 
 author = driver.find_elements_by_id('author-text')
-print(author[0].text)
 df = pd.DataFrame(data)
-print(df.head())
+df.to_csv('data.csv', sep=',', index=False)
