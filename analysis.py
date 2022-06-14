8702773990e861d64db6f9dd70a106314fa50aba
eliczi <57072657+eliczi@users.noreply.github.com>
2022-06-14 11:47:14 +0200

    file deletion

--- analysis.py
+++ analysis.py
@@ -13,8 +13,7 @@ import gensim
 
 # ys = YouTubeScraper('https://www.youtube.com/watch?v=54kz9zv_080')
 # ys.scrape()
-# data = ys.comments
-# replies = ys.comments['replies']  # list of dictionaries, 10th comment replies is 10th list element as a dict
+# data = ys.comments# replies = ys.comments['replies']  # list of dictionaries, 10th comment replies is 10th list element as a dict
 # data.pop('replies', None)
 # df = pd.DataFrame(data)
 # df.to_csv('df.csv')
